c39af8b24f25cdd66be19a1120f3e1ae1b525881
orlanobel <orlando.bel.jimmy@gmail.com>
2019-08-21 09:27:45 -0500

    formularios p1

---
+++ tablasYformulrios/resultado.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Tablas</title>
+    </head>
+    <body>
+        <?php
+            $n = $_POST['nombre'];
+            $p = $_POST["pass"];
+
+            echo "nombre: ".$n."<br/>";
+            echo "contraseña: ".$p."<br/>";
+        ?>
+    </body>
+</html>
